35ca5f222e8b8c2d0b2e08c4112355762e6e68eb
Pedro <pa90887@gmail.com>
2016-12-06 21:55:39 -0200

    fomulario

--- src/LivrariaBundle/Entity/Produtos.php
+++ src/LivrariaBundle/Entity/Produtos.php
@@ -3,22 +3,189 @@
 
 namespace LivrariaBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+
 /**
  * Description of Produtos
  *
  * @author aluno
+ * @ORM\Entity
+ * @ORM\Table(name="produtos")
  */
+
+
 class Produtos 
 {
+    /** @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * 
+     */
     private $id;
-    
+  
+  
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     private $nome;
     
+    /**
+     *
+     * @ORM\Column(type="integer", length=5)
+     */
     private $quantidade;
     
+    /**
+     *
+     * @ORM\Column(type="decimal", scale=2)
+     */        
+    private $preco;
+    
+    
+    /**
+     *
+     * @ORM\Column(type="string")
+     */    
     private $tipo;
     
+    /**
+     *
+     * @ORM\Column(type="string")
+     */    
     private $image;
     
     
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     *
+     * @return Produtos
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set quantidade
+     *
+     * @param integer $quantidade
+     *
+     * @return Produtos
+     */
+    public function setQuantidade($quantidade)
+    {
+        $this->quantidade = $quantidade;
+
+        return $this;
+    }
+
+    /**
+     * Get quantidade
+     *
+     * @return integer
+     */
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    /**
+     * Set preco
+     *
+     * @param string $preco
+     *
+     * @return Produtos
+     */
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+
+        return $this;
+    }
+
+    /**
+     * Get preco
+     *
+     * @return string
+     */
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    /**
+     * Set tipo
+     *
+     * @param string $tipo
+     *
+     * @return Produtos
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    /**
+     * Get tipo
+     *
+     * @return string
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Produtos
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
