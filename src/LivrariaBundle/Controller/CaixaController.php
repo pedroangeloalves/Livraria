4e021ff704544c1ca8145bc29a22626224f5fc94
Pedro <pa90887@gmail.com>
2016-12-14 21:51:32 -0200

    final do curso

--- src/LivrariaBundle/Controller/CaixaController.php
+++ src/LivrariaBundle/Controller/CaixaController.php
@@ -18,7 +18,7 @@ use \LivrariaBundle\Entity\Produtos;
 class CaixaController extends Controller
 {
     /**
-     * @Route("/caixa")
+     * @Route("/caixa", name="caixa-index")
      */
     public function pdvAction(Request $request)
     {
@@ -50,28 +50,23 @@ class CaixaController extends Controller
      */     
     public function carregarProdutoAction(Request $request)
     {
-        
         $em = $this->getDoctrine()->getManager();
         
         $codProd = $request->request->get('codigo');
-        
-        // dump($produto);die();
-        
+              
         $cupomId = $request->getSession()->get('cupom-id');
+        
         $produto = $em->getRepository('LivrariaBundle:Produtos')
                 ->find($codProd);        
+        
         $cupom = $em->getRepository('LivrariaBundle:Cupom')
                 ->find($cupomId);
 
-        
         $quantItem = $em->getRepository('LivrariaBundle:CupomItem')
                 ->findBy(array("cupomId" => $cupomId));
         
-      //  dump($quantItem);die();
-        
         if ($produto instanceof Produtos)
         {
-        
             $novoItem = new CupomItem();
             $novoItem->setCupomId($cupom);
             $novoItem->setDescricaoItem($produto->getNome());
@@ -86,6 +81,8 @@ class CaixaController extends Controller
             $retorn["status"] = "ok";
             $retorn["produtos"] = $produto;
             
+            return $this->json($retorn);
+            
         } else {
             
             $retorn["status"] = "erro";
@@ -95,7 +92,6 @@ class CaixaController extends Controller
         
         return $this->json($retorn);
 
-        
     }
     
     /**
@@ -103,36 +99,40 @@ class CaixaController extends Controller
      */          
     public function estornarItemAction(Request $request, $item)
     {
-        $cupomId = $$request->getSession()->get('cupom-id');
+        $cupomId = $request->getSession()->get('cupom-id');        
         $em = $this->getDoctrine()->getManager();
         
-        $itemOld = $em->getRepository('LivrariaBundle;CupomItem')
-                ->findBy(array(
+        $itemOld = $em->getRepository('LivrariaBundle:CupomItem')
+                ->findOneBy(array(
                    'cupomId' => $cupomId,
-                    'ordemItem' => $item                    
-                ));
+                   'ordemItem' => $item                    
+                ));        
+        $cupom = $em->getRepository('LivrariaBundle:Cupom')
+                ->find($cupomId);        
+        
+        $quantItem = $em->getRepository('LivrariaBundle:CupomItem')
+                ->findBy(array("cupomId" => $cupomId));        
         
         $itemEstorno = new CupomItem();
-        $itemEstorno->setCupomId($cupomId);
-        $itemEstorno->getDescricaoItem("Estorno do Item: $item");
-        $itemEstorno->setItemCod(1001);
+        $itemEstorno->setCupomId($cupom);
+        $itemEstorno->setDescricaoItem("Estorno do Item: $item");
+        $itemEstorno->setItemCod(9999);
         $itemEstorno->setQuantidade(1);
-        $itemEstorno->setValorUnitario($itemOld->getValorunitario() * -1);
-        
+        $itemEstorno->setValorUnitario($itemOld->getValorUnitario() * -1);
+        $itemEstorno->setOrdemItem(count($quantItem)+1);
+                
         $em->persist($itemEstorno);
         $em->flush();
         
-        return $this->json('ok');
-        
-        
+        return $this->redirectToRoute('caixa-index');                
     }
     
     /**
-     * @Route("/caixa/cancelar")
+     * @Route("/caixa/cancelar", name="cancelar")
      */    
     public function cancelarVendaAction(Request $request)
     {
-        $cupomId = $$request->getSession()->get('cupom-id');
+        $cupomId = $request->getSession()->get('cupom-id');
         $em = $this->getDoctrine()->getManager();
         $cupom = $em->getRepository('LivrariaBundle:Cupom')->find($cupomId);
         
@@ -141,12 +141,14 @@ class CaixaController extends Controller
         $em->persist($cupom);
         $em->flush();
         
-        return $this->json('OK');
+        $request->getSession()->set('cupom-id', null);
+        
+        return $this->redirectToRoute('caixa-index');
         
     }
 
     /**
-     * @Route("/caixa/finalizar")
+     * @Route("/caixa/finalizar", name="concluir")
      */    
     public function finalizarVendaAction(Request $request)
     {
@@ -155,15 +157,36 @@ class CaixaController extends Controller
         $cupom = $em->getRepository('LivrariaBundle:Cupom')->find($cupomId);
         
         $cupom->setStatus('FINALIZADO');
+
+        $valorTotal = 0;
+        
+        $itens = $em->getRepository("LivrariaBundle:CupomItem")
+                ->findBy(array(
+                        "cupomId" => $request->getSession()->get('cupom-id')
+                        ));        
+        foreach ($itens as $item)
+        {
+
+            //  - Fechar o total da Compra - ok 
+            $valorTotal += $item->getValorUnitario();
+
+            //  - baixar os itens do estoque
+            $produto = $em->getRepository('LivrariaBundle:Produtos')
+                    ->find($item->getItemCod());
+            $produto->setQuantidade($produto->getQuantidade() - 1);
+            $em->persist($produto);                        
+        }
+        
+        $cupom->setValorTotal($valorTotal);
         
         $em->persist($cupom);
         $em->flush();
         
-        // Pendencias :
-        //  - baixar os itens do estoque
-        //  - Fechar o total da Compra
+        $request->getSession()->set('cupom-id', null);
         
-        return $this->json('OK');
+        
+        
+         return $this->redirectToRoute('caixa-index');
         
     }
         
---
+++ src/LivrariaBundle/Entity/User.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace LivrariaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="usuarios")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */    
+    protected $id;
+    
+    /**
+     *
+     * @ORM\Column(type="string")
+     */
+    protected $nome;
+    
+    
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     *
+     * @return User
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+}
